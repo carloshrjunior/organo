65f57b3e44f2df19049cfaf3348e2d46fca5bb0d
unknown <carlosh.rjunior.com>
2022-07-28 12:46:24 -0300

    Upload banner

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -4,7 +4,7 @@ import './Banner.css';
 const Banner = () => {
   return (
     <header className="banner">
-      <img src="/img/banner.png" alt="Banner principal"/>
+      <img src="./img/banner.png" alt="Banner principal"/>
     </header>
   )
 }
